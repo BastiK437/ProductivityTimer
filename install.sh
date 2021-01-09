d26b609332bf5c6cca9cbc9995f82f7699569bfe
Bastian Krammer <bastiankrammer@googlemail.com>
2021-01-09 12:44:44 +0100

    add installer script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PROGRAM_DIR=Program
+
+while [ true ]
+do
+    read -p "Enter install dir (full path): " INSTALL_DIR
+
+    if [[ ! -d "$INSTALL_DIR" ]]
+    then
+        echo "$INSTALL_DIR not exists on your filesystem."
+    else 
+        break;
+    fi
+
+done 
+
+if [[ ! -d "$INSTALL_DIR/ProductivityTimer" ]]
+then
+    mkdir $INSTALL_DIR/ProductivityTimer
+fi
+
+cp -r $PROGRAM_DIR/* $INSTALL_DIR/ProductivityTimer/
+
+FILE="productivity.desktop"
+
+/bin/cat <<EOM >$FILE
+[Desktop Entry]
+Name=Productivity Timer
+Comment=Little stopwatch application to measure productivity on each day
+Exec=$INSTALL_DIR/ProductivityTimer/start.sh
+Icon=$INSTALL_DIR/ProductivityTimer/icon.png
+Terminal=false
+Type=Application
+EOM
+
+mv productivity.desktop $HOME/.local/share/applications/
